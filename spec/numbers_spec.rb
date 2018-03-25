ee9dbd262bfb1d18fca7a7cad30bfa2b18a4f683
Anton Katunin <antulik@gmail.com>
2018-03-25 12:07:17 +1100

    Add more specs for number one

--- spec/numbers_spec.rb
+++ spec/numbers_spec.rb
@@ -17,6 +17,18 @@ RSpec.describe RubyNumbers::Numbers do
 
       expect(klass.one(operation)).to eq(-1)
     end
+
+    it 'accepts times 5 operation and returns 5' do
+      operation = RubyNumbers::Operation.new(:*, 5)
+
+      expect(klass.one(operation)).to eq 5
+    end
+
+    it 'accepts divided_by 0 operation and raises error' do
+      operation = RubyNumbers::Operation.new(:/, 0)
+
+      expect { klass.one(operation) }.to raise_exception(ZeroDivisionError)
+    end
   end
 
   describe '#two' do
