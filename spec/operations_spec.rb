ddfd018819c79bfb4b4d7b055bdb2f6912f646d3
Anton Katunin <antulik@gmail.com>
2018-03-25 11:57:03 +1100

    Add more operations specs

--- spec/operations_spec.rb
+++ spec/operations_spec.rb
@@ -25,11 +25,19 @@ RSpec.describe RubyNumbers::Operations do
     it 'returns operation' do
       expect(klass.divided_by(1)).to be_a RubyNumbers::Operation
     end
+
+    it 'has "/" type' do
+      expect(klass.divided_by(1).type).to eq :/
+    end
   end
 
   describe '#times' do
     it 'returns operation' do
       expect(klass.times(1)).to be_a RubyNumbers::Operation
+    end
+
+    it 'has "*" type' do
+      expect(klass.times(1).type).to eq :*
     end
   end
 end
