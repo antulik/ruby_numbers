6718ee2969a053f6825e36b93b24edcab5295da4
Anton Katunin <antulik@gmail.com>
2018-03-25 11:52:28 +1100

    Add minus, divided_by and times operation specs

--- spec/operations_spec.rb
+++ spec/operations_spec.rb
@@ -6,4 +6,22 @@ RSpec.describe RubyNumbers::Operations do
       expect(klass.plus(1)).to be_a RubyNumbers::Operation
     end
   end
+
+  describe '#minus' do
+    it 'returns operation' do
+      expect(klass.minus(1)).to be_a RubyNumbers::Operation
+    end
+  end
+
+  describe '#divided_by' do
+    it 'returns operation' do
+      expect(klass.divided_by(1)).to be_a RubyNumbers::Operation
+    end
+  end
+
+  describe '#times' do
+    it 'returns operation' do
+      expect(klass.times(1)).to be_a RubyNumbers::Operation
+    end
+  end
 end
