5a27f26c674accff8bb10d6a0f7a0f103254aec9
Anton Katunin <antulik@gmail.com>
2018-03-25 11:53:29 +1100

    Make tests pass

--- lib/ruby_numbers/operations.rb
+++ lib/ruby_numbers/operations.rb
@@ -2,4 +2,16 @@ module RubyNumbers::Operations
   def plus(number)
     RubyNumbers::Operation.new(:+, number)
   end
+
+  def minus(number)
+    RubyNumbers::Operation.new(:+, number)
+  end
+
+  def divided_by(number)
+    RubyNumbers::Operation.new(:+, number)
+  end
+
+  def times(number)
+    RubyNumbers::Operation.new(:+, number)
+  end
 end
